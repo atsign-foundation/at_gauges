c39e455ad4aac64d7125b090474d0e1273023deb
Curtly Critchlow <curtlycritchlow@ccwc.io>
2022-09-26 07:55:05 -0400

    linear gauge division can be determine by user

---
+++ at_linear_gauges/example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:at_linear_gauges/at_linear_gauges.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Linear gauge Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Scaffold(
+          body: SizedBox(
+        child: Center(
+          child: SimpleLinearGauge(
+            divisions: 10,
+          ),
+        ),
+      )),
+    );
+  }
+}
