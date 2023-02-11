78b92a72fa95e9f196fe2345eb39927bbf09791e
project2021-code <project2021.code@gmail.com>
2023-02-12 04:42:57 +0900

    Updated Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,28 +5,18 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'At Gauges',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyGauges(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyGauges extends StatelessWidget {
-  const MyGauges({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
 
-  @override
-  Widget build(BuildContext context) {
+  double _maxValue = 50;
+
+  Widget myGauges() {
     return Scaffold(
       // appBar: AppBar(title: const Text('Radial Gauges')),
       body: SafeArea(
@@ -52,7 +42,7 @@ class MyGauges extends StatelessWidget {
               maxValue: 100,
               actualValue: 70,
               // Optional Parameters
-              minValue: 0,
+              minValue: -0,
               size: 400,
               title: Text('Scale Radial Gauge'),
               titlePosition: TitlePosition.top,
@@ -87,12 +77,147 @@ class MyGauges extends StatelessWidget {
               majorTickStrokeWidth: 3,
               minorTickStrokeWidth: 3,
               actualValueTextStyle:
-                  const TextStyle(color: Colors.black, fontSize: 15),
+              const TextStyle(color: Colors.black, fontSize: 15),
               majorTickValueTextStyle: const TextStyle(color: Colors.black),
+            ),
+            const SegmentRadialGauge(
+              maxValue: -50,
+              actualValue: -120,
+              // Optional Parameters
+              minValue: -150,
+              size: 250,
+              title: Text('Zone Radial Gauge'),
+              titlePosition: TitlePosition.top,
+              pointerColor: Colors.cyan,
+              needleColor: Colors.cyan,
+              decimalPlaces: 2,
+              isAnimate: true,
+              animationDuration: 100,
+              unit: TextSpan(
+                text: 'V',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.cyan,
+                ),
+              ),
+              isPointer: true,
+              titleText: "Title",
+              title2Text: "Title2",
+            ),
+            SegmentRadialGauge(
+              maxValue: 100,
+              actualValue: 50,
+              // Optional Parameters
+              minValue: 35,
+              size: 250,
+              title: const Text('Zone Radial Gauge'),
+              titlePosition: TitlePosition.top,
+              pointerColor: Colors.cyan,
+              needleColor: Colors.cyan,
+              decimalPlaces: 0,
+              isAnimate: true,
+              animationDuration: 100,
+              unit: const TextSpan(
+                text: 'V',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.cyan,
+                ),
+              ),
+              isPointer: true,
+              titleText: "Title",
+              title2Text: "Title2",
+              segmentList: [
+                ZoneSegment(
+                  size: 1,
+                  color: Colors.red.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 2,
+                  color: Colors.blue.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 3,
+                  color: Colors.orangeAccent.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 4,
+                  color: Colors.greenAccent.withOpacity(0.95),
+                ),
+              ],
+            ),
+            SegmentRadialGauge(
+              maxValue: 12,
+              actualValue: _maxValue,
+              // Optional Parameters
+              minValue: -4,
+              size: 250,
+              title: const Text('Zone Radial Gauge'),
+              titlePosition: TitlePosition.top,
+              pointerColor: Colors.cyan,
+              needleColor: Colors.cyan,
+              decimalPlaces: 2,
+              isAnimate: true,
+              animationDuration: 100,
+              unit: const TextSpan(
+                text: 'V',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.cyan,
+                ),
+              ),
+              isPointer: true,
+              titleText: "Title",
+              title2Text: "Title2",
+              segmentStartAngle: 135,
+              segmentSweepAngle: 270,
+              segmentMainNo: 8,
+              segmentSubNo: 5,
+              segmentList: [
+                ZoneSegment(
+                  size: 1,
+                  color: Colors.red.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 2,
+                  color: Colors.blue.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 1,
+                  color: Colors.orangeAccent.withOpacity(0.95),
+                ),
+                ZoneSegment(
+                  size: 4,
+                  color: Colors.greenAccent.withOpacity(0.95),
+                ),
+              ],
+            ),
+            Slider(
+              value: _maxValue,
+              min: -290,
+              max: 90,
+              onChanged: (value) {
+                setState(() {
+                  _maxValue = value;
+                });
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'At Gauges',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: myGauges(),
     );
   }
 }
